fc3b32165ba8431bd0b2f3558dc42560ba8def2f
BrunnoM7 <brunno.marchetti7@gmail.com>
2020-07-12 19:02:47 -0300

    first commit

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php $home = get_template_directory_uri(); ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?= $home ?>/reset.css">
+  <link rel="stylesheet" href="<?= $home ?>/style.css">
+  <title>Document</title>
+  <?php wp_head(); ?>
+</head>
+<body>
